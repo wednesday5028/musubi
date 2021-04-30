a2ed32b2c3f130c1179e3990a837e7d8bffc0b00
King <wednesday5028@gmail.com>
2021-04-30 17:29:39 +0900

    first commit

---
+++ src/main/java/com/kiminoRankwa/musubi/MusubiApplication.java
@@ -0,0 +1,13 @@
+package com.kiminoRankwa.musubi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusubiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusubiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiminoRankwa/musubi/MusubiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiminoRankwa.musubi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusubiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
